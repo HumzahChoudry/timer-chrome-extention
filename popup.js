8a2faa8534c1263e1b8f41c0a44bc30d118151f4
eal6gf <elum1234@gmail.com>
2018-04-21 18:15:34 -0400

    remove unused code

--- popup.js
+++ popup.js
@@ -1,21 +1,4 @@
-// let changeColor = document.getElementById('changeColor');
-//
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-//
-// changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
-
 function timerSubmit(event) {
-  console.log('clicked')
   const minutes = parseInt(document.getElementsByClassName('input')[0].value);
   const seconds = parseInt(document.getElementsByClassName('input')[1].value);
   const reminder = parseInt(document.getElementsByClassName('input')[2].value);
@@ -37,7 +20,7 @@ function startTimer(duration) {
         var display = document.querySelector('#time');
         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) { 
+        if (--timer < 0) {
             timer = duration;
         }
     }, 1000);
