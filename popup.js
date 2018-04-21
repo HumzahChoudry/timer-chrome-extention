d7e4673123a14261f80fa6fbe72ed2f2ab3a4755
eal6gf <elum1234@gmail.com>
2018-04-21 18:49:33 -0400

    almost workes

--- popup.js
+++ popup.js
@@ -4,26 +4,38 @@ function timerSubmit(event) {
   const reminder = parseInt(document.getElementsByClassName('input')[2].value);
   startTimer(convertTime(minutes, seconds), reminder);
 
+}
 
 function convertTime (minutes, seconds){
-  return minutes_to_seconds = minutes * 60 + seconds
+  // return minutes * 60 + seconds
+  return seconds
 }
 
 function startTimer(duration, reminder) {
+    function stopTimers(){
+      clearInterval(myInterval)
+    }
+
     var timer = duration, minutes, seconds;
-    setInterval(function () {
+    var audio = new Audio('sound.mp3')
+    var myInterval = setInterval(function() {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
-
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         var display = document.querySelector('#time');
         display.textContent = minutes + ":" + seconds;
-
+        console.log(duration, seconds, minutes)
         if (--timer < 0) {
-            timer = duration;
+            timer = duration
+            stopTimers()
+        }
+
+        if (seconds == reminder){
+          audio.play()
         }
 
 
     }, 1000);
+
 }
