10363925680a6a9690e4b31f13b2884c8376f490
github username <github email address>
2018-04-21 17:53:30 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,52 @@
+// let changeColor = document.getElementById('changeColor');
+//
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+//
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//         tabs[0].id,
+//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
+
+function timerSubmit = (event) => {
+  debugger
+  const minutes = parseInt(document.getElementsByClassName('input')[0].value);
+  const seconds = parseInt(document.getElementsByClassName('input')[1].value);
+  const reminder = parseInt(document.getElementsByClassName('input')[2].value);
+
+  handleRunTimer(minutes, seconds, reminder);
+}
+
+function handleRunTimer = (minutes, seconds, reminder) => {
+  minutes_to_seconds = minutes * 60
+
+}
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
