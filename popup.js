1b5c72e5d27f1aaee4112c06f54a9b817dcf4ef2
humzahchoudry <Humzah.choudry@gmail.com>
2019-04-10 20:28:38 -0400

    adding error handling

--- popup.js
+++ popup.js
@@ -1,18 +1,3 @@
-// let changeColor = document.getElementById('changeColor');
-//
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-//
-// changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
 var countInterval;
 document.querySelector("#timer-submit").addEventListener("click", timerSubmit);
 document.querySelector("#timer-clear").addEventListener("click", clearTimer);
@@ -22,7 +7,7 @@ function timerSubmit(event) {
   const minutes = parseInt(document.getElementsByClassName("input")[0].value);
   const seconds = parseInt(document.getElementsByClassName("input")[1].value);
   const reminder = parseInt(document.getElementsByClassName("input")[2].value);
-  handleRunTimer(minutes, seconds, reminder);
+  checkErrors(minutes, seconds, reminder);
 }
 
 function handleRunTimer(minutes, seconds, reminder) {
@@ -70,3 +55,62 @@ window.onload = function() {
   const seconds = (document.getElementsByClassName("input")[1].value = 0);
   const reminder = (document.getElementsByClassName("input")[2].value = 30);
 };
+
+function checkErrors(minutes, seconds, reminder) {
+  //check for blanks
+  if (isNaN(minutes) || isNaN(seconds) || isNaN(reminder)) {
+    alert("Minutes, Seconds, and Reminder at fields must be numbers.");
+    console.error(
+      "Minutes, seconds, and reminder must all be numbers. Use zeroes if you have to.s"
+    );
+  }
+  handleRunTimer(minutes, seconds, reminder);
+}
+
+// function timerSubmit(event) {
+//   const minutes = parseInt(document.getElementsByClassName('input')[0].value, 10);
+//   const seconds = parseInt(document.getElementsByClassName('input')[1].value, 10);
+//   const reminder = parseInt(document.getElementsByClassName('input')[2].value, 10);
+//   checkErrors(minutes,seconds,reminder)
+//   console.log(minutes,seconds,reminder)
+//   startTimer(convertTime(minutes, seconds), reminder);
+// }
+//
+// function convertTime (minutes = 0, seconds = 0){
+//   // converts time value in seconds for
+//   return minutes * 60 + seconds
+// }
+//
+// function startTimer(duration, reminder = 60) {
+//     // stop timer callback to end the timer
+//     function stopTimer(){
+//       clearInterval(myInterval)
+//     }
+//
+//     function checkPlayReminder(minutes,seconds,reminder, audioElement) {
+//       if ( convertTime(minutes,seconds) == reminder){
+//         audioElement.play()
+//       }
+//     }
+//
+//     //shorthand for declaring a bunch of variables
+//     var timer = duration, minutes, seconds;
+//     var audio = new Audio('sound.mp3')
+//
+//     var myInterval = setInterval(function() {
+//         minutes = parseInt(timer / 60, 10)
+//         seconds = parseInt(timer % 60, 10);
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+//         var display = document.querySelector('#time');
+//         display.textContent = minutes + ":" + seconds;
+//
+//         if (--timer < 0) {
+//             timer = duration
+//             stopTimer()
+//         }
+//
+//         checkPlayReminder(minutes,seconds,reminder, audio)
+//
+//
+//     }, 1000);
